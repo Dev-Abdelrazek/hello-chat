aa8a78779e5d9574248acb14923464daa3e4785e
Dev-Abdelrazek <abdelrazek.ali.abdelrazek@gmail.com>
2021-07-20 06:12:29 +0200

    add video call

--- sockets/chat.socket.js
+++ sockets/chat.socket.js
@@ -11,5 +11,11 @@ module.exports = (io) => {
         cb();
       });
     });
+    socket.on("requestPeerId", (chatId) => {
+      socket.broadcast.to(chatId).emit("getPeerId");
+    });
+    socket.on("sendPeerId", (data) => {
+      socket.broadcast.to(data.chatId).emit("recievePeerId", data.peerId);
+    });
   });
 };
