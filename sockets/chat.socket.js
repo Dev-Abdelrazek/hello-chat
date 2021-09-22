34b46e77814c08ae99dd2c2868482b4e60f3654c
abdelrazekAli <dev.abdelrazek@gmail.com>
2021-09-22 11:02:37 +0200

    Added video call

--- sockets/chat.socket.js
+++ sockets/chat.socket.js
@@ -5,12 +5,21 @@ module.exports = (io) => {
     socket.on("joinChat", (chatId) => {
       socket.join(chatId);
     });
-
+    // Send message
     socket.on("sendMsg", (msg, cb) => {
       newMessage(msg).then(() => {
         io.to(msg.chat).emit("newMsg", msg);
         cb();
       });
+    });
+
+    // Video call
+    socket.on("requestPeerId", (chatId) => {
+      socket.broadcast.to(chatId).emit("getPeerId");
+    });
+
+    socket.on("sendPeerId", (data) => {
+      socket.broadcast.to(data.chatId).emit("recievePeerId", data.peerId);
     });
   });
 };
